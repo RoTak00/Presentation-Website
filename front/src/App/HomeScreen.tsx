c3f4a79221523f4070a9255f9a2a705aa4599cb4
Robert Takacs <RoTak@protonmail.com>
2023-06-23 21:14:04 +0300

    Remove Main

--- front/src/App/HomeScreen.tsx
+++ front/src/App/HomeScreen.tsx
@@ -1,46 +1,10 @@
-import NavigationBar from "../Components/Navigation";
-import ImageCarousel from "../Components/ImageCarousel";
-import "./styles/HomeScreen.css";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-
-import { CarouselImageType } from "../Utils/Types";
-import ProjectMenu from "../Components/ProjectMenu";
-import ContactMenu from "../Components/ContactMenu";
-import ContactIcons from "../Components/ContactIcons";
-import CustomFooter from "../Components/Footer";
 
 const HomeScreen = () => {
-  const images: CarouselImageType[] = [
-    {
-      imageName: "carousel-new-1.jpg",
-    },
-    {
-      imageName: "carousel-new-2.jpg",
-    },
-    {
-      imageName: "carousel-tech.jpg",
-    },
-
-    {
-      imageName: "carousel-new-4.jpg",
-    },
-    {
-      imageName: "carousel-future.jpg",
-    },
-  ];
+  
   return (
-    <div
-      style={{
-        backgroundImage: `url(/images/background.png)`,
-        backgroundRepeat: "repeat-y",
-      }}
-    >
-      <NavigationBar />
-      <ImageCarousel images={images} delay={2000} />
-      <ProjectMenu />
-      <ContactMenu />
-      <ContactIcons />
-      <CustomFooter />
+    <div>
+      
+      
     </div>
   );
 };
--- front/src/AppRouter.tsx
+++ front/src/AppRouter.tsx
@@ -1,17 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 
 import HomeScreen from "./App/HomeScreen";
-import ErasmusApplicationScreen from "./App/ErasmusApplicationScreen";
-import ProjectsScreen from "./App/ProjectsScreen";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route index element={<HomeScreen />} />
 
-      <Route path="projects/page/:pageParam" element={<ProjectsScreen />} />
-      <Route path="projects" element={<ProjectsScreen />} />
-      <Route path="esn" element={<ErasmusApplicationScreen />} />
     </Routes>
   );
 };
